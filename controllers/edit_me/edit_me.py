70ac269cf9c8e6f58d3034307f025b0fc264cac0
TotallyRandomGus <115017569+TotallyRandomGus@users.noreply.github.com>
2022-11-02 11:33:29 +0100

    Update edit_me.py

--- controllers/edit_me/edit_me.py
+++ controllers/edit_me/edit_me.py
@@ -4,17 +4,18 @@ from controller import Robot
 import sys
 
 # Define the target motor position in radians.
-target = 10
+left_target = 12
+right_target = 8
 
 # Get pointer to the robot.
 robot = Robot()
 
-# Print the program output on the console
-print("Move the motors of the Thymio II to position " + str(target) + ".")
+motor_max_speed = robot.getDevice("motor.left").getMaxVelocity()
+robot.getDevice("motor.right").setVelocity(motor_max_speed*right_target/left_target)
 
 # Set the target position of the left and right wheels motors.
-robot.getDevice("motor.left").setPosition(target)
-robot.getDevice("motor.right").setPosition(target+2)
+robot.getDevice("motor.left").setPosition(left_target)
+robot.getDevice("motor.right").setPosition(right_target)
 
 # This is the most simple controller that works for this benchmark
 # If you want to experiment with more complex functions, you can read the programming guide here:
